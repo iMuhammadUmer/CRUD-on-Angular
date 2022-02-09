968b80c47e8048ece7d433cda4a4c95653ec7868
Muhammad Umer <mumer1297@gmail.com>
2022-02-09 15:45:00 +0500

    :zap: Navigation
    add routers for navigation

--- src/app/topbar/topbar-routing.module.ts
+++ src/app/topbar/topbar-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {AddProductComponent} from '../products/add-product/add-product.component';
+import {ViewProductsComponent} from '../products/view-products/view-products.component';
+
+const routes: Routes = [
+  {
+    path: 'addProduct',
+    component: AddProductComponent
+  },
+  {
+    path: 'viewProducts',
+    component: ViewProductsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
